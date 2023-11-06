21635d8adf55250b66ca92804e7c2a26a2c1873e
vaishgithubvdj <vaishjambhule@gmail.com>
2023-11-06 13:35:27 +0530

    commit changes

---
+++ src/pack1/class111.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class class111 {
+	
+	  public static void main(String[] args)    {
+		  
+		  int a = 90;
+		  int b = 67;
+		  
+		  System.out.println(a+b);
+		  System.out.println(a-b);
+		  System.out.println(a*b);
+	  }
+}
