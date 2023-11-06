0acbd0c57708940707ba1e2d638764d82ef36555
vaishgithubvdj <vaishjambhule@gmail.com>
2023-11-06 15:16:33 +0530

    commit changes

---
+++ src/pack3/class3.java
@@ -0,0 +1,14 @@
+package pack3;
+
+public class class3 {
+	
+	public static void main(String[] args)   {
+		
+		int a = 67;
+		int b = 89;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
