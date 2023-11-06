339182ff119bb29926c067276862ef5e8cf9304b
vaishgithubvdj <vaishjambhule@gmail.com>
2023-11-06 15:00:34 +0530

    commit changes

---
+++ src/pack2/class22.java
@@ -0,0 +1,13 @@
+package pack2;
+
+public class class22 {
+
+	public static void main(String[] args) {
+	
+		int r = 89;
+		int t = 55;
+		
+		System.out.println(r-t);
+        System.out.println(r*t);
+	}
+}
